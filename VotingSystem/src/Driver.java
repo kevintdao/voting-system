fa614f28461e9bfe1656bd9ebe672f121bad1c77
cole-garton <cole-garton@uiowa.edu>
2020-12-03 13:26:13 -0600

    Adding Frame, Driver, LandingPage, and Vote

---
+++ VotingSystem/src/Driver.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Driver {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}
---
+++ VotingSystem/src/Frame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+    public Frame() {
+        super("voting system");
+    }
+}
