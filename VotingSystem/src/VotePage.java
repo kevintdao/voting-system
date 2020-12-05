350c1eb88776e2d030611ec890e9035a3609c6db
truongdao <kevin-dao@uiowa.edu>
2020-12-05 11:19:45 -0600

    update vote page GUI

--- VotingSystem/src/VotePage.java
+++ VotingSystem/src/VotePage.java
@@ -8,11 +8,14 @@ public class VotePage extends JPanel {
     private VoteTab[] tabArray;
     
     JButton returnToHome;
+    JButton voteButton;
 
     //this needs to have a number passed in and then make as many pages as necessary
     public VotePage() {
-        setLayout(new BorderLayout());
+        setLayout(new GridBagLayout());
         setName("Vote");
+        GridBagConstraints c = new GridBagConstraints();
+
         Options.getLanguageComboBox(4).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
@@ -25,7 +28,27 @@ public class VotePage extends JPanel {
                 refreshPanel();
             }
         });
-        add(Options.getLanguageComboBox(4),BorderLayout.PAGE_START);
+        c.insets = new Insets(10,20,0,20);
+        c.gridx = 2;
+        c.gridy = 0;
+        c.weightx = 0.2;
+        c.ipady = 10;
+        c.anchor = GridBagConstraints.FIRST_LINE_END;
+        add(Options.getLanguageComboBox(4), c);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 0.5;
+        c.ipady = 10;
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        add(new JLabel(""), c);
+
+        c.gridx = 1;
+        c.gridy = 0;
+        c.weightx = 0.5;
+        c.ipady = 10;
+        c.anchor = GridBagConstraints.PAGE_START;
+        add(new JLabel(""), c);
 
         JTabbedPane tabs = new JTabbedPane();
         tabArray = new VoteTab[5]; //this will be updated with same number as below
@@ -34,7 +57,15 @@ public class VotePage extends JPanel {
             tabArray[i] = new VoteTab(5); //this will be updated from database
             tabs.add(title,tabArray[i]);
         }
-        add(tabs, BorderLayout.CENTER);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 1.0;
+        c.ipady = 80;
+        c.gridwidth = 3;
+        c.insets = new Insets(10,10,0,10);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.CENTER;
+        add(tabs, c);
 
         returnToHome = new JButton(returnHomeLang[Options.getLanguageIndex()]);
         returnToHome.addActionListener(new ActionListener() {
@@ -44,7 +75,21 @@ public class VotePage extends JPanel {
                 //this needs to update the progress bar
             }
         });
-        add(returnToHome, BorderLayout.PAGE_END);
+        c.gridx = 0;
+        c.gridy = 2;
+        c.weightx = 1.0;
+        c.ipady = 10;
+        c.gridwidth = 1;
+        c.insets = new Insets(10,20,0,20);
+        add(returnToHome, c);
+
+        voteButton = new JButton("Submit Vote");
+        c.gridx = 1;
+        c.gridy = 2;
+        c.weightx = 1.0;
+        c.ipady = 10;
+        c.gridwidth = 2;
+        add(voteButton, c);
     }
 
     private void refreshPanel(){
