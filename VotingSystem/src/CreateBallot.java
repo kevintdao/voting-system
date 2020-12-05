1a99cd80f7a1c1a9b0cca5ff4224a8e1da72a7f7
timevans <timothy-evans@uiowa.edu>
2020-12-05 13:03:51 -0600

    added CreateBallot page

--- VotingSystem/src/CreateBallot.java
+++ VotingSystem/src/CreateBallot.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CreateBallot extends JPanel {
-//TODO: add languageComboBox[7]
+    //TODO: language functionality
     private String[] returnHomeLang = {"Return to Home", "Vuelve a casa", "Retourner à la maison"};
     private String[] candidatePositions = {"President", "Representative", "Congress", "Governor", "Mayor", "Sheriff"};
     JButton returnToHome;
@@ -14,14 +14,40 @@ public class CreateBallot extends JPanel {
     private JTextArea showCandidatesArea;
     private final JComboBox<String> positionJComboBox;
     private JButton addButton;
-    private HashMap<String, String> candidates;
+    private HashMap<String, ArrayList<String>> candidates;
 
     //this needs to have a numher passed in and then make as many pages as necessary
     public CreateBallot() {
         setLayout(new BorderLayout());
         setName("Vote");
 
-        candidates = new HashMap<>();
+        candidates = new HashMap<>(); //key: position, value: array of candidates running for position
+
+        positionJComboBox = new JComboBox(candidatePositions);
+        positionJComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox comboBox = (JComboBox) e.getSource();
+                String position = comboBox.getSelectedItem().toString();
+                showCandidatesArea.setText(""); //clear text
+                for(String i : candidates.keySet()){ //show candidates for selected position
+                    if(i == position){
+                        for(int j = 0; j < candidates.get(i).size(); j++){
+                            showCandidatesArea.append(candidates.get(i).get(j));
+                            showCandidatesArea.append("\n");
+                        }
+
+                    }
+                }
+                refreshPanel();
+            }
+        });
+        //set up hash map to store the candidates entered for each position
+        for(int i = 0; i < positionJComboBox.getItemCount(); i++){
+            ArrayList<String> a = new ArrayList<>();
+            candidates.put(positionJComboBox.getItemAt(i), a);
+        }
+
 
         Options.getLanguageComboBox(5).addActionListener(new ActionListener() { //TODO: change to 6
             @Override
@@ -36,11 +62,9 @@ public class CreateBallot extends JPanel {
             }
         });
 
-
+        //From fig26_47_48
         Box box = Box.createHorizontalBox(); // create box
-        String demo = "This is a demo string to\n" +
-                "illustrate copying text\nfrom one textarea to \n" +
-                "another textarea using an\nexternal event\n";
+        String demo = "This is a demo string";
 
         enterCandidateArea = new JTextArea(demo, 10, 15);
         box.add(new JScrollPane(enterCandidateArea)); // add scrollpane
@@ -50,14 +74,21 @@ public class CreateBallot extends JPanel {
         addButton.addActionListener(
                 new ActionListener() // anonymous inner class
                 {
-                    // appened text from enterCandidatesArea to showCandidatesArea
+                    // append text from enterCandidatesArea to showCandidatesArea
                     @Override
                     public void actionPerformed(ActionEvent event)
                     {
                         showCandidatesArea.append(enterCandidateArea.getText() + "\n");
+                        ArrayList<String> candidatesArray = candidates.get(positionJComboBox.getSelectedItem().toString());
+                        candidatesArray.add(enterCandidateArea.getText());
 
-                        candidates.put(positionJComboBox.getSelectedItem().toString(), enterCandidateArea.getText());
                         enterCandidateArea.setText(""); //clear
+                        for(String i : candidates.keySet()) {
+                            System.out.println(i + ": ");
+                            for(int j = 0; j < candidates.get(i).size(); j++){
+                                System.out.println(candidates.get(i).get(j));
+                            }
+                        }
                     }
                 } // end anonymous inner class
         ); // end call to addActionListener
@@ -70,26 +101,10 @@ public class CreateBallot extends JPanel {
 
 
 
-        positionJComboBox = new JComboBox(candidatePositions);
-        positionJComboBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JComboBox comboBox = (JComboBox) e.getSource();
-                String position = comboBox.getSelectedItem().toString();
-                showCandidatesArea.setText(""); //clear text
-                for(String i : candidates.keySet()){ //show candidates for that position
-                    if(i == position){
-                        showCandidatesArea.append(i + "/n");
-                    }
-                }
 
-                refreshPanel();
-
-            }
-        });
         add(positionJComboBox, BorderLayout.PAGE_START);
 
-        //add(Options.getLanguageComboBox(4),BorderLayout.PAGE_START); //TODO: change to 6
+        //add(Options.getLanguageComboBox(4),BorderLayout.PAGE_START); // change to 6
         returnToHome = new JButton(returnHomeLang[Options.getLanguageIndex()]);
         returnToHome.addActionListener(new ActionListener() {
             @Override
@@ -106,6 +121,7 @@ public class CreateBallot extends JPanel {
         repaint();
     }
     private void insertCandidates(){
-
+        //TODO: insert candidates into candidates db with candidateID autoincremented
+        //TODO: electionID is obtained from the county of the auditor
     }
 }
